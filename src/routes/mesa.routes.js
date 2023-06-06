e8182670f9eaeb47ec12e511cef6ff7074171457
enriq16 <enriq16@gmail.com>
2023-06-05 23:40:47 -0400

    agrego el route para buscar mesa por restaurante

--- src/routes/mesa.routes.js
+++ src/routes/mesa.routes.js
@@ -5,6 +5,7 @@ import {
   createMesa,
   deleteMesa,
   getMesa,
+  getMesaByRestaurante,
   getMesas,
   updateMesa,
 } from "../controllers/mesaDAO.controller.js";
@@ -15,6 +16,7 @@ const router = Router();
 // rutas
 router.get("/mesa", getMesas);
 router.get("/mesa/:id", getMesa);
+router.get("/mesa/restaurante/:id", getMesaByRestaurante);
 router.post("/mesa", createMesa);
 router.put("/mesa/:id", updateMesa);
 router.delete("/mesa/:id", deleteMesa);
